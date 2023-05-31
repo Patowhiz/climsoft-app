70a14451e793d845a40c0cfd9b03329a319bd16f
patowhiz <conlooptechnologies@gmail.com>
2023-05-31 14:17:55 +0300

    added datepicker

--- src/app/modules/shared/controls/date-picker/date-picker.component.ts
+++ src/app/modules/shared/controls/date-picker/date-picker.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.scss']
+  styleUrls: ['./date-picker.component.css']
 })
 export class DatePickerComponent implements OnInit {
+
+  maxDate = new Date();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDateChanged( event: MatDatepickerInputEvent<Date>) {
+    console.log("date",event.value);
   }
 
 }
